a250581bf459de55768d5754aad5a264571a4a35
apk-fueled <apk@fueled.co>
2013-08-02 11:58:02 +0530

    solution text is displayed on web page

--- js/tween.js
+++ js/tween.js
@@ -79,6 +79,7 @@ function compareArrays(source, target, print) {
 	
 }
 
+// calculates and returns array of total distance values 
 function findAvgDistance(array) {
 	var dist_all = new Array();
 	for(var i=0; i < array.length; i++) {
@@ -87,12 +88,13 @@ function findAvgDistance(array) {
 			var dist = compareArrays(array[i],array[j],false);
 			tmp_dist = tmp_dist + dist.length;
 		}
-		dist_all[dist_all.length] = tmp_dist/600;
+		dist_all[dist_all.length] = tmp_dist; ///600
 	}
 
 	return dist_all;
 }
 
+// returns index of smallest value in array
 function findSmallest(dist) {
 	var smallest = dist[0];
 	var smallestIndex = 0;
@@ -103,29 +105,160 @@ function findSmallest(dist) {
 			smallestIndex = i;
 		}
 	}
-	alert('found smallest');
 
 	return smallestIndex ;
 
 }
 
+// creates and returns array of terminal nodes 
+function findTerminals(relation) {
+	var terminals = new Array();
+    //document.write(" Terminals <br/>");
+    for (var i=0; i < relation.length ; i++){
+        if(relation[i].length === 1){
+           	//document.write(i+" : "+relation[i]+" <br/>");
+           	terminals[terminals.length] = i ;
+         }
+    }
+    return terminals;
+}
+
+// returns genetically nearest node with distance
+function findParent(geneticData, orphan, index) {
+	var nearestDist = compareArrays(orphan[index], geneticData[0],false).length;;
+	var nearestIndex = 0;
+	for(i= 1; i< geneticData.length && orphan.indexOf(i) < 0; i++) {
+		var dist = compareArrays(orphan[index], geneticData[i],false).length;
+		if(nearestDist > dist) {
+			nearestDist = dist;
+			nearestIndex = i;
+		}
+	}
+	var near = new Array();
+	near[0] = nearestDist;
+	near[1] = nearestIndex;
+	return near;
+}
+
+
 function main() {
 	
 	
-	var relation = new Array(600);
+	var relation = new Array();
 
-	var offset = new Array(600);
+	var offset = new Array();
 
 	var avg = new Array();
 
 	var geneticData = loadFileIntoArray('tweens600.txt');
-
-	var avgDistance = findAvgDistance(geneticData);
-	writeArray(avgDistance);
-
-	var smallest = findSmallest(avgDistance);
-
-	document.write(" Smallest Index "+smallest+" <br/>");
+	//document.write("geneticData size"+geneticData.length+"<br/>");
 
 	
-}
+	var avgDistance = findAvgDistance(geneticData);
+	//writeArray(avgDistance);
+	var smallest = findSmallest(avgDistance);
+	document.write("By least average genetic difference method  Root Node --> "+smallest+" <br/>");
+	
+
+	
+    	for (var  i = 0; i < geneticData.length ; i++) {
+ 
+        	var tmp = new Array();
+        	for (var j = 0; j< geneticData.length ; j++) {   //i+1
+
+	        		var offsettmp = new Array();
+	             	if(i != j){
+	             		var diff = compareArrays(geneticData[i],geneticData[j],false);
+	             		
+	                 		if(diff.length <= 147){ //147
+	                     	
+	                     		tmp[tmp.length] = j;
+	                     		offsettmp[offsettmp.length] = diff.length - 120;
+	                     	}
+	                 
+	                 		
+	             	}
+ 			}
+ 			relation[i] = tmp;
+	        offset[i] = offsettmp;
+		}
+	
+     //calculate average distance
+     for(var i=0; i< offset.length; i++)
+     {
+         if(offset[i].length!=0)
+        {
+             var total=0;
+            for(var j=0; j< offset[i].length; j++)
+                total+= offset[i][j];
+ 
+             avg[i] = total/offset[i].length;
+         }
+     }
+ 
+ 	
+
+    var population = integerArray(600);
+    var parent = new Array();
+    //document.write(population+"<br/>");
+
+    while(population.length > 1) {
+    	//finding terminals
+	    var terminals = new Array();
+	    terminals = findTerminals(relation);
+	    //document.write("<br/> Terminals "+terminals.length+"->"+terminals+" <br/>");
+
+	 	// Deleting terminals from relation
+
+	 	for(var i=0; i < relation.length; i++) {
+	 		if(relation[i].length > 0) {
+	 			for(var j=0; j < relation[i].length; j++) {
+	 				if(terminals.indexOf(relation[i][j]) >= 0) {
+	 					//population.splice(relation[i][j],1);
+	 					relation[i].splice(j,1);
+	 				}
+	 			}
+	 		}
+	 	}
+
+	 	for(var i=0; i<terminals.length; i++) {
+
+	 		if(isNaN(parent[terminals[i]]))
+		 		parent[terminals[i]] = relation[terminals[i]][0];
+
+	 		var index = population.indexOf(terminals[i]);
+	 		if( index >= 0) {
+	 			population.splice(index,1);
+	 		}
+	 	}
+	 	
+    }
+
+    parent[population[0]] = -1;		// -1 means it is root of tree
+
+    
+    //check for any node without parent
+    var orphan = new Array();
+    for(var i=0; i < parent.length; i++) {
+    	if(isNaN(parent[i])) {
+    		orphan[orphan.length] = i;
+    		parent[i] = -2;
+    	}
+    }
+    /*
+    //Find nearest distance of each orphan in with rest rest of nodes
+    var nearNode_dist = new Array();
+    for(var i=0; i< orphan.length; i++) {
+    	nearNode_dist[nearNode_dist.length] = findParent(geneticData,orphan,i);
+    }
+    document.write(" for orphans"+nearNode_dist+" <br/>");
+    */
+
+    document.write("By successiver terminal node elimination method Root Node ->"+ population[0] +"<br/>");
+    document.write("Child Parent Relationship <br/>");
+    document.write("Child : Parent <br/>");
+    writeArray(parent);
+
+    //drawGraph();
+
+ }
---
+++ readme.md
@@ -0,0 +1,14 @@
+To see the answer open index.html in firefox web browser.
+
+Time complexity is Big Oh of n^2
+
+I am assuming, for cloned tweens, bits are copied with 75.5% accuracy and not 80% accuracy.
+I have found the root by two methods method 
+1)finding a node which has least genetic difference with all other nodes.
+2)continuesly finding terminal nodes and deleting them, its like reaching root by moving from bottom to top.
+
+Using both these methods answer coming out is node numbered 365.
+
+I have found out child parent relationship too. There is one disconnected component.
+
+Among all 600 nodes[0...599] there are 4 nodes 
